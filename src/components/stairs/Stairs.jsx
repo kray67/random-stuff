13fc190b8eac882ee634161faf6332af965cf992
João Dias <joao.dias@inm.pt>
2024-03-25 19:41:38 +0000

    messing with stairs anim

---
+++ src/components/stairs/Stairs.jsx
@@ -0,0 +1,37 @@
+import { motion } from 'framer-motion'
+import { opacity, expand } from './anim'
+import './Stairs.scss'
+
+const Stairs = ({children, backgroundColor}) => {
+
+    const anim = (variants, custom=null) => {
+        return {
+            initial: "initial",
+            animate: "enter",
+            exit: "exit",
+            custom,
+            variants
+        }
+    }
+
+    const nbOfColumns = 5
+    return (
+        <div className='page stairs' style={{backgroundColor}}>
+            <motion.div {...anim(opacity)} className='transition-background'/>
+            <div className='transition-container'>
+                {
+                    [...Array(nbOfColumns)].map( (_, i) => {
+                        return (
+                            <motion.div key={i} {...anim(expand, nbOfColumns - i)}/>
+                        ) 
+                    })
+                }
+            </div>
+            {
+                children
+            }
+        </div>
+    )
+}
+
+export default Stairs
