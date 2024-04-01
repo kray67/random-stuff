9549e1e3c5ee925448e978666702fb19c5567d5c
João Dias <joao.dias@inm.pt>
2024-04-01 08:45:57 +0100

    still trying to get blob to render only curves

--- src/assets/scripts/blob.js
+++ src/assets/scripts/blob.js
@@ -3,7 +3,6 @@ import "path-data-polyfill"
 export const animateBlob = (svg) => {
     const path = svg.querySelector('path')
     const INITIAL_SEGMENTS = path.getPathData()
-    console.log(INITIAL_SEGMENTS)
 
     const startAnimationCycle = (segments) => {
         const newSegments = updateSegmentValues(segments)
@@ -15,23 +14,37 @@ export const animateBlob = (svg) => {
 
         setTimeout(() => {
             startAnimationCycle(INITIAL_SEGMENTS)
-        }, 1000);
+        }, 500);
     }
 
     const updateSegmentValues = (segments) => {
+        // return segments.map(segment => {
+        //     if (segment.type === 'C') {
+        //         const updatedValues = segment.values.map(value => getRandomNumber(value))
+        //         return { ...segment, values: updatedValues }
+        //     }
+        //     return segment
+        // })
         return segments.map(segment => {
-            if (segment.type === 'C') {
-                const updatedValues = segment.values.map(value => getRandomNumber(value))
-                return { ...segment, values: updatedValues }
-            }
-            return segment
+            const updatedValues = segment.values.map(value => getRandomNumber(value))
+            return { ...segment, values: updatedValues }
         })
     }
 
     const getRandomNumber = (initialValue) => {
-        const min = Math.max(-100, initialValue - 15)
-        const max = Math.min(100, initialValue + 15)
-        return Math.floor(Math.random() * (max - min + 1)) + min
+        let min = Math.max(1, initialValue - 15)
+        let max = Math.min(99, initialValue + 15)
+
+        if (initialValue < 0) {
+            min = Math.max(-99, initialValue - 15)
+            max = Math.min(-1, initialValue + 15)
+        }
+
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+
+        // const min = Math.max(-100, initialValue - 15)
+        // const max = Math.min(100, initialValue + 15)
+        // return Math.floor(Math.random() * (max - min + 1)) + min
     }
 
     startAnimationCycle(INITIAL_SEGMENTS)
